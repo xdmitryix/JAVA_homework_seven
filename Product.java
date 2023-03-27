f5e69044263c1c2add8931ba257bc5cb5758b1df
dmitry <rbdeman@yandex.ru>
2023-03-27 23:36:34 +0300

    создал product

--- Product.java
+++ Product.java
@@ -1,3 +1,40 @@
 public class Product {
-    
+    private String name;
+    private double price;
+    private int counter;
+
+    public Product(String name, double price, int counter){
+        this.name = name;
+        this.price = price;
+        this.counter = counter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s : %.2f : %d", name, price, counter);
+    }
 }
---
+++ main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product product1 = new Product("молоко", 200, 3);
+        System.out.println(product1.toString());
+    }
+}
