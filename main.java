8ed027d5b9e9ba2e29ff6692a7c76a237835a84d
dmitry <rbdeman@yandex.ru>
2023-03-28 00:30:43 +0300

    добавтл товары

--- main.java
+++ main.java
@@ -2,7 +2,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        Product product1 = new Product("молоко", 200, 3);
-        System.out.println(product1.toString());
+        Milk milk1 = new Milk("Домик в деревне", 200, 3);
+        System.out.println(milk1.toString());
+        Chocolate choko1 = new Chocolate("Ritter sport", 200 , 4, "Молочный");
+        System.out.println(choko1.toString());
+        DrinksGas drinkGas1 = new DrinksGas("Coca-cola", 250, 1);
+        System.out.println(drinkGas1.toString());
+        DrinksWithOutGas drinkWOG1 = new DrinksWithOutGas("Bon aqua", 80, 10);
+        System.out.println(drinkWOG1.toString());
     }
 }
