b3b0ff56600ed69310cd26ca07ab5711551b5924
dmitry <rbdeman@yandex.ru>
2023-03-28 20:04:43 +0300

    заполнил массив

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class VendingMachine {
-    
+    private List <Product> products = new ArrayList<>();
+
+    public VendingMachine addProduct(Product productAdd){
+        products.add(productAdd);
+        return this;
+    }
 }
--- main.java
+++ main.java
@@ -2,13 +2,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        Milk milk1 = new Milk("Домик в деревне", 200, 3);
-        System.out.println(milk1.toString());
-        Chocolate choko1 = new Chocolate("Ritter sport", 200 , 4, "Молочный");
-        System.out.println(choko1.toString());
-        DrinksGas drinkGas1 = new DrinksGas("Coca-cola", 250, 1);
-        System.out.println(drinkGas1.toString());
-        DrinksWithOutGas drinkWOG1 = new DrinksWithOutGas("Bon aqua", 80, 10);
-        System.out.println(drinkWOG1.toString());
+        VendingMachine mart = new VendingMachine();
+        mart.addProduct(new Product("Блинчики", 229, 4))
+                .addProduct(new Milk("Домик в деревне", 89, 3))
+                .addProduct(new Chocolate("Ritter sport", 180, 6, "С орехом"))
+                .addProduct(new Drinks("Coca Cola", 120, 2))
+                .addProduct(new DrinksGas("Aqua minerale", 80,3))
+                .addProduct(new DrinksWithOutGas("Aqua minerale", 89, 4));
+        System.out.println(mart);
+
     }
 }
